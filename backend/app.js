6fd13b4e615eba93fcfda2a197ccd7ceb34f9fa6
satyyam01 <satyamnautiyal74@gmail.com>
2025-07-04 04:19:34 +0530

    test-prod: config updates for prod testing

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ mongoose.connection.once('open', () => {
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 app.use(bodyParser.json());
